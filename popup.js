a2199197e309b0bddd1a87ca09698122352c1236
jake-waratahs <jakebloom744@gmail.com>
2016-11-14 18:55:38 +1100

    Better solution for facebook pages and generalised adding messages

--- popup.js
+++ popup.js
@@ -1,24 +1,33 @@
-chrome.runtime.sendMessage({
-    action: 'get_source'
-})
+function addMessages(messages) {
+    // Remove the 'Loading text'
+    var uiWrapper = document.getElementById('container');
+    var load = document.getElementById('loading');
+    uiWrapper.removeChild(load);
+
+    // Add the messages from alexjs to the list
+    var messageContainer = document.getElementById('messages');
+    messages.forEach(function(message){
+        messageContainer.innerHTML += '<div class="item">' + message + '</div>';
+    })
+
+    // If there's nothing to report, tell them
+    if (messages.length === 0) {
+        messageContainer.innerHTML += '<div class="item">No insensitive language found. You\'re awesome! 😃</div>';
+    }    
+}
+
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (tabs[0].url.match(/.*facebook\.com.*/)) {
+        addMessages(['Unfortunately we don\'t currently support Facebook pages 😞']);
+    } else {
+        chrome.runtime.sendMessage({
+            action: 'get_source'
+        });
+    }
+});
 
 chrome.runtime.onMessage.addListener(function(request){
     if (request.action === 'analysis_complete') {
-
-        // Remove the 'Loading text'
-        var uiWrapper = document.getElementById('container');
-        var load = document.getElementById('loading');
-        uiWrapper.removeChild(load);
-
-        // Add the messages from alexjs to the list
-        var messageContainer = document.getElementById('messages');
-        request.analysis.forEach(function(message){
-            messageContainer.innerHTML += '<div class="item">' + message + '</div>';
-        })
-
-        // If there's nothing to report, tell them
-        if (request.analysis.length === 0) {
-            messageContainer.innerHTML += '<div class="item">No insensitive language found. You\'re awesome!</div>';
-        }
+        addMessages(request.analysis);
     }
-})
+});
